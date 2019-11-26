95b75f0557f4194eed4dd67044b1088bde427329
Shridhar Saurin Shah <42818678+Shridhar-2205@users.noreply.github.com>
2019-11-26 08:23:16 -0800

    Update NeedleFind.java

--- NeedleFind.java
+++ NeedleFind.java
@@ -1,19 +1,19 @@
-public int strStr(String haystack, String needle) {
-        if(needle.length() == 0) {
-            return 0;
-        }
-        int ptr1 = 0;
-        for(int i = 0; i<haystack.length(); i++) {
-            if(haystack.charAt(i) == needle.charAt(ptr1)) {
-                if(ptr1 == needle.length()-1) {
-                    return i-ptr1;
-                }
-                ptr1++;
-            }
-            else if(ptr1 != 0) {
-                i = i-ptr1;
-                ptr1 = 0;
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle == "" || haystack == "") return 0;
+        int n = needle.length();
+        int h = haystack.length();
+        for(int i = 0; i <= h - n; i++){
+            if(haystack.substring(i, i + n ).equals(needle)){
+            return i;
             }
         }
         return -1;
     }
+}
+
+
+//TC:O(h)
+//SC:O(1)
+
+//Here first we will calcualte the length of the two strings, then we will traverse till haystack-needle and compare the substring from the beginning index to the size of needle with with the needle.
