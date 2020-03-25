6b9ee4b85a8018067e8a9c934183c669ca3bcf8a
priyanka1012-singhal <priyanka.singhal@sjsu.edu>
2020-03-24 23:05:31 -0700

    Add KMP algo for strstr()

--- Problem1.java
+++ Problem1.java
@@ -1,6 +1,6 @@
 // Problem1  Implement strStr() (https://leetcode.com/problems/implement-strstr/)
 
-//Time comlexity O(hlen (hlen - nlen)) 
+//Time complexity O(hlen (hlen - nlen)) 
 class Solution {
     public int strStr(String haystack, String needle) {
         if(needle.isEmpty()) return 0;
@@ -13,3 +13,78 @@ class Solution {
         return -1;
     }
 }
+
+// KMP
+//Time complexity O(slength + plength) 
+// Space O(plength) 
+// where slength -  string length and plength = pattern length
+class Solution {
+    public int strStr(String haystack, String needle) {
+        //System.out.print(Arrays.toString(computePI("issip")));
+        if(needle.isEmpty() && haystack.isEmpty()) return 0;
+        if(needle.isEmpty()) return 0;
+        // KMP
+        //populate prefix-suffix array
+        int[] pi = computePI(needle);
+        
+        int i = 0, j = 0, hlen = haystack.length(), nlen = needle.length();
+        
+        while(i < hlen){
+           
+            if(haystack.charAt(i) == needle.charAt(j)){
+                // match
+                j++;
+                i++;
+                
+                
+            }
+            if(j == nlen){
+                return i - j;
+            }
+            
+            if(i < hlen && haystack.charAt(i) != needle.charAt(j)){
+                // no match
+                if(j > 0){
+                    j = pi[j - 1]; 
+                }else{
+                    i++;
+                }
+            }
+            
+            
+            
+            
+             
+        }
+        
+        return -1;
+    }
+    //Populate prefix and suffix array
+    private int[] computePI(String pattern) { 
+        int n = pattern.length();
+        int[] pi = new int[n];
+        pi[0] = 0;
+    
+        int j = 0, i = 1;
+    
+        while(i < n) {
+           
+            if(pattern.charAt(i) == pattern.charAt(j)) {
+                j++;
+                pi[i] = j;
+                i++;
+            } else {
+                
+                if(j == 0) {
+                    pi[i] = 0;
+                    i++;
+                } else {
+                    j = pi[j-1];
+                }
+            }
+        }
+    
+        return pi;
+    }
+}
+  
