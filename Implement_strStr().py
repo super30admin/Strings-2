bf9508b4de78ca6c6c1d9cebaa7c0830bbc60ccb
TriptiSantani19 <71227108+TriptiSantani19@users.noreply.github.com>
2020-11-03 18:59:06 -0500

    Create Implement_strStr().py

---
+++ Implement_strStr().py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Algorithm - Sliding window concept
+
+Step 1: Declare lengthNeedle (to store length of needle), lengthHaystack(to store length of haystack)
+        and windowStartPos(start position of window) and windowEndPos (end position of window) and initialize it with
+        0 and lengthNeedle respectively for first iteration in while loop.
+Step 2: Check for the base case if lengthNeedle == 0, return 0
+Step 3: 
+
+"""
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+      
+        lengthNeedle = len(needle) 
+        lengthHaystack = len(haystack)
+        
+        windowStartPos = 0 # Starting position of window
+        windowEndPos = lengthNeedle # ending position of window
+        # base case
+        if(lengthNeedle == 0):
+            return 0
+        while (windowStartPos <= lengthHaystack - lengthNeedle):   
+            if (haystack[windowStartPos:windowEndPos] == needle):
+                return windowStartPos
+            else:
+                windowStartPos = windowStartPos +1
+                windowEndPos = windowEndPos + 1
+            
+        return -1
+            
+        
