2d907d3c6676db9bc035541834d162b3cf4492db
TriptiSantani19 <71227108+TriptiSantani19@users.noreply.github.com>
2020-11-03 19:16:17 -0500

    Update Implement_strStr().py

--- Implement_strStr().py
+++ Implement_strStr().py
@@ -6,7 +6,19 @@ Step 1: Declare lengthNeedle (to store length of needle), lengthHaystack(to stor
         and windowStartPos(start position of window) and windowEndPos (end position of window) and initialize it with
         0 and lengthNeedle respectively for first iteration in while loop.
 Step 2: Check for the base case if lengthNeedle == 0, return 0
-Step 3: C
+Step 3: Start a while loop and iterate through it till the condition windowStartPos <= lengthHaystack - lengthNeedle
+        is true
+Step 4: Check if the string from the starting position of window i.e. windowStartPos till windowEndPos is same as string needle
+        if yes, return the window starting position i.e. windowStartPos else increment windowStartPos and windowEndPos by 1
+Step 5: Once we are out of loop, that means we did not find any string same as expected, so we return -1 in that case
+
+Time Complexity - O(N)
+Space Complexity - O(1)
+
+Code accepted in leetcode
+
+Doubt - Is writing "haystack[windowStartPos:windowEndPos] == needle" this way considered as language feature or is it fine 
+        to use this way
 
 """
 
