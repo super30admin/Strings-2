99a5aa2e58970594e71d487c19cacc596432e5cc
Rujal <rujal8452@gmail.com>
2022-05-21 01:00:49 -0400

    Problem2

---
+++ Problem2.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Problem2 {
+    public List<Integer> findAnagrams(String s, String p) {
+        if (s == null || p == null || s.length() < p.length()) return new ArrayList<>();
+
+        Map<Character, Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < p.length(); i++) {
+            char c = p.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int match = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char in = s.charAt(i);
+            if (map.containsKey(in)) {
+                int count = map.get(in);
+                count--;
+                if (count == 0) {
+                    match++;
+                }
+                map.put(in, count);
+            }
+            if (i >= p.length()) {
+                char out = s.charAt(i - p.length());
+                if (map.containsKey(out)) {
+                    int count = map.get(out);
+                    count++;
+                    if (count == 1) {
+                        match--;
+                    }
+                    map.put(out, count);
+                }
+            }
+            if (match == map.size()) {
+                result.add(i - p.length() + 1);
+            }
+        }
+        return result;
+    }
+}
