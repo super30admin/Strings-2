2672ad837794ec2facc9e5d7f204e1c71ec8c1aa
Rujal <rujal8452@gmail.com>
2022-05-21 23:48:36 -0400

    Problem1

--- Problem1.java
+++ Problem1.java
@@ -51,4 +51,31 @@ public class Problem1 {
         }
         return result;
     }
+
+    // BrutForce
+    // TC : O(n^2)
+    // SC : O (1)
+    public int strStr1(String haystack, String needle) {
+        if (haystack == null || haystack.length() == 0) return 0;
+        if (needle == null || needle.length() == 0) return 0;
+
+        int m = haystack.length();
+        int n = needle.length();
+
+        int i = 0;
+        while (i <= m - n) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                int j = 1;
+                int k = i + 1;
+
+                while (j < n && k < m && haystack.charAt(k) == needle.charAt(j)) {
+                    j++;
+                    k++;
+                }
+                if (j == n) return i;
+            }
+            i++;
+        }
+        return -1;
+    }
 }
