6a36ce6d7ca9d329c919d486d42ecdf541b978f7
jayachandravvs <42566670+jayachandravvs@users.noreply.github.com>
2022-06-14 22:53:35 -0500

    Update ImplementStr.java

--- ImplementStr.java
+++ ImplementStr.java
@@ -1,1 +1,23 @@
-
+// o(n) time || o(1) space
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0)
+            return 0;
+        if (needle.length() > haystack.length())
+            return -1;
+        int hayIndex = 0;
+        while (hayIndex != haystack.length()) {
+            int i = hayIndex;
+            int j = 0;
+            while (j < needle.length() && i < haystack.length() && 
+                   haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+            }
+            if (j == needle.length())
+                return hayIndex;
+            hayIndex++;
+        }
+        return -1;
+    }
+}
