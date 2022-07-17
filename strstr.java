d9530d4ae58782e93f9f0f7929e946e54263168c
akshaykmus <akshaykmus@gmail.com>
2022-07-17 15:24:47 -0400

    Completed Implement str str problem

--- strstr.java
+++ strstr.java
@@ -22,3 +22,53 @@ class Solution {
 
     }
 }
+
+// T: O(m)
+// S : O(m)
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int m = haystack.length();
+        int n = needle.length();
+        if (m < n)
+            return -1;
+        int i = 0;
+        int j = 0;
+        int[] lps = lps(needle);
+        while (i < m) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+                if (j == n)
+                    return i - n;
+            } else if (haystack.charAt(i) != needle.charAt(j) && j > 0) {
+                j = lps[j - 1];
+            } else if (haystack.charAt(i) != needle.charAt(j) && j == 0) {
+                i++;
+            }
+        }
+        return -1;
+
+    }
+
+    private int[] lps(String needle) {
+        int[] lps = new int[needle.length()];
+        int j = 0;
+        int i = 1;
+        lps[0] = 0;
+        while (i < needle.length()) {
+            if (needle.charAt(i) == needle.charAt(j)) {
+                j++;
+                lps[i] = j;
+                i++;
+            } else if (needle.charAt(i) != needle.charAt(j) && j > 0) {
+                j = lps[j - 1];
+            } else if (needle.charAt(i) != needle.charAt(j) && j == 0) {
+                lps[i] = 0;
+                i++;
+            }
+        }
+        return lps;
+
+    }
+
+}
