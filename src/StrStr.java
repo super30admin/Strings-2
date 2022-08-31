97c09f84b49a36f11901a10abff58cf0728aa43a
SRaval20 <raval.sagar.sr@gmail.com>
2022-08-31 16:56:18 -0700

    updated the code

---
+++ src/StrStr.java
@@ -0,0 +1,61 @@
+//                   LPS       Match      Total
+// Time Complexity:  O(n)       O(m)      O(m+n)
+// Space Complexity: O(n)       O(1)       O(n)
+// where m = length of haystack, n = length of needle
+
+class StrStr {
+    public int strStr(String haystack, String needle) {
+
+        int[] lps = getLPSarr(needle);
+        int i=0;
+        int j=0;
+
+        while(i < haystack.length() && j < needle.length()) {
+            char hChar = haystack.charAt(i);
+            char nChar = needle.charAt(j);
+
+            if(hChar == nChar) {
+                i++;
+                j++;
+            }
+            else {
+                if(j == 0) {
+                    i++;
+                }
+                else {
+                    j = lps[j-1];
+                }
+            }
+        }
+
+        if(j == needle.length()) {
+            return i - j;
+        }
+        return -1;
+    }
+
+    private int[] getLPSarr(String needle) {                      // Longest Prefix Suffix array
+        int[] lps = new int[needle.length()];
+        int l = 0;
+        int r = 1;
+        while(r < needle.length()) {
+            char lChar = needle.charAt(l);
+            char rChar = needle.charAt(r);
+
+            if(lChar == rChar) {
+                lps[r] = l + 1;
+                l++;
+                r++;
+            }
+            else {
+                if(l == 0) {
+                    r++;
+                }
+                else {
+                    l = lps[l-1];
+                }
+            }
+        }
+        return lps;
+    }
+}
