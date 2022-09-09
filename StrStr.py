0888c3cf242b8fe3b20e4fbb8a313935f441f1e3
Raj Shah <rajshah1998.d@gmail.com>
2022-09-09 09:00:33 -0400

    Completed Strings-2

---
+++ StrStr.py
@@ -0,0 +1,39 @@
+"""
+Time Complexity : O(len(haystack) * len(needle))
+Space Complexity : O(1)
+"""
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(haystack) < len(needle):
+            return -1
+
+        store = 0
+        i = 0 #haystack
+        j = 0 #needle
+
+        while i <= len(haystack) - len(needle):
+            #print("i", i)
+            if haystack[i] != needle[0]:
+                i += 1
+
+            else:
+                store = i
+                j = 0
+                while j < len(needle):
+                    if haystack[i] == needle[j]:
+                        j += 1
+                        i += 1
+                        #print(i, j)
+
+                    else:
+                        i = store + 1
+                        j = 0
+                        break
+
+                #print("here", store, j)
+                if j == len(needle):
+                    #print("h", store)
+                    return store
+                else:
+                    j = 0
+        return -1
