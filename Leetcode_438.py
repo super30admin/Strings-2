8307410dacf29e00b9dc1fbdd1d1f706b3129048
Rutvik Avaiya <rutvikavaiya01@gmail.com>
2022-09-23 06:11:50 -0500

    Sprint Completed

---
+++ Leetcode_438.py
@@ -0,0 +1,25 @@
+#Time Complexity: O(n)
+#Space Complexity: O(1)
+
+from collections import Counter
+
+
+class Solution:
+    def findAnagrams(self, s, p) :
+        ans=[]
+        count=Counter(p)
+        required=len(p)
+        
+        for r,c in enumerate(s):
+            count[c]-=1
+            if count[c]>=0:
+                required-=1
+            if r>=len(p):
+                count[s[r-len(p)]] += 1
+                if count[s[r-len(p)]]>0:
+                    required+=1
+            if required==0:
+                ans.append(r-len(p)+1)
+        return ans
+    
+      
