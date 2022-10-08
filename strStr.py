2afd827cd6d68a6885c2f5c000259f64432e27f2
Janam Parikh <janamparikh08@gmail.com>
2022-10-08 18:04:05 -0400

    Completed Strings-2

---
+++ strStr.py
@@ -0,0 +1,15 @@
+#Time: O(n+m)
+#Space: O(m)
+#Problem ran on leetcode successfully
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i: i + len(needle)] == needle:
+                return i
+        
+        return -1
