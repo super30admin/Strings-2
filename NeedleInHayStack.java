4e337649bcd5c5297a4cfafdbd6efe7a7ed4063f
kulkarnisneha <sneha.kulkarni@verizonconnect.com>
2023-07-12 18:53:06 -0400

    Completed Strings-2

--- NeedleInHayStack.java
+++ NeedleInHayStack.java
@@ -1,39 +1,69 @@
-// Time Complexity : O(n) where n is the length of the string haystack
-// Space Complexity :O(n) n is the length of the string haystack
-// Did this code successfully run on Leetcode : Yes
+
 // Any problem you faced while coding this :
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class NeedleInHayStack {
+
+    /// Time Complexity : O(m*n) where m is the length of the string haystack and n is the length of  the needle
+    //// Space Complexity :O(1) as no auxiliary space is used
+    //// Did this code successfully run on Leetcode : Yes
     public int strStr(String haystack, String needle) {
-        Queue<Integer> q = new LinkedList<>();
 
-        for(int i =0; i<haystack.length(); i++){
+        int m = haystack.length();
+        int n = needle.length();
+
+        if(m < n) return -1;
+
+        int i =0;
+        while(i < m-n+1){
             if(haystack.charAt(i) == needle.charAt(0)){
-                q.add(i);
-            }
-        }
-
-        int index = -1, first = -1, start = -1, i = -1;
-        while(!q.isEmpty()){
-            i = 0;
-            first = q.poll();
-            start = first;
-            while(i< needle.length() && first < haystack.length()){
-                if(haystack.charAt(first) != needle.charAt(i)){
-                    break;
+                int j =0; int k = i;
+                while(haystack.charAt(k) == needle.charAt(j)){
+                    k++; j++;
+                    if(j == n){
+                        return i;
+                    }
                 }
-
-                first++;
-                i++;
             }
-            if(i == needle.length()){
-                index = start;
-                break;
-            }
-
+            i++;
         }
-        return index;
+        return -1;
     }
+
+    /// Time Complexity : O(m+n) where m is the length of the string haystack and n is the length of  the needle
+    //// Space Complexity :O(1) as no auxiliary space is used
+    //// Did this code successfully run on Leetcode : Yes
+    public int strStr2(String haystack, String needle) {
+
+        int m = haystack.length();
+        int n = needle.length();
+        int k = 26;
+        long factor = (long)(Math.pow(k, n));
+
+        if(m < n) return -1;
+
+        long needleHash = 0l;
+        for(int i =0; i< n; i++){
+            needleHash = needleHash*k + (needle.charAt(i)-'a'+1);
+        }
+
+        long currentHash = 0;
+        for(int i =0; i< m; i++){
+            //in
+            Character in = haystack.charAt(i);
+            currentHash = currentHash*k + (in-'a'+1);
+            if(i >= n){
+                //out
+                Character out = haystack.charAt(i-n);
+                currentHash = currentHash - (factor * (out-'a'+1));
+            }
+
+            if(currentHash == needleHash){
+                return i - n +1;
+            }
+        }
+        return -1;
+    }
+
 }
