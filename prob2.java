bd9346684f212f683d8e0a614f8936a9b557738b
DhyeyaVyas10 <132728547+DhyeyaVyas10@users.noreply.github.com>
2023-08-17 20:39:18 -0400

    Completed Strings 2

---
+++ prob2.java
@@ -0,0 +1,36 @@
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res=new ArrayList<>();
+        Map<Character, Integer> pHash = new HashMap<>();
+        Map<Character, Integer> hash = new HashMap<>();
+        for (int i = 0; i < p.length(); i++) {
+            pHash.put(p.charAt(i), pHash.getOrDefault(p.charAt(i), 0) + 1);
+        }
+        int left = 0, right = 0;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            hash.put(c, hash.getOrDefault(c, 0) + 1);
+            right++;
+            if (pHash.equals(hash)) {
+                res.add(left);
+            }
+            if (right - left == p.length()) {
+                char leftChar = s.charAt(left);
+                if (hash.containsKey(leftChar)) {
+                    hash.put(leftChar, hash.get(leftChar) - 1);
+                    if (hash.get(leftChar) == 0) {
+                        hash.remove(leftChar);
+                    }
+                }
+                left++;
+            }
+        }
+
+        return res;
+    }
+}
