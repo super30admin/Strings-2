6eb33cd725f66ec9e473a7c082bbb35a42ba17bc
Sagar Raval <62638370+SRaval20@users.noreply.github.com>
2023-10-02 00:58:28 -0700

    Update StrStr.java

--- src/StrStr.java
+++ src/StrStr.java
@@ -59,3 +59,29 @@ class StrStr {
         return lps;
     }
 }
+
+
+
+// ******************** BRUTE FORCE APPROACH ********************
+//                   LPS
+// Time Complexity:  O(m*n)
+// Space Complexity: O(1)
+// where m = length of haystack, n = length of needle
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+
+        if(haystack.length() >= needle.length()) {
+            for(int i=0; i<haystack.length()-needle.length()+1; i++) {
+                int j=0;
+                while(j < needle.length() && haystack.charAt(i+j) == needle.charAt(j)) {
+                    j++;
+                }
+                if(j==needle.length())
+                    return i;
+            }
+        }
+
+        return -1;
+    }
+}
