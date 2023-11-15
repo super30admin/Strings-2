93335dfac4d12368392bcd1d031c00dfb4dc9c64
Yatish Koul <36380820+yatishkoul@users.noreply.github.com>
2023-11-15 12:46:56 -0800

    Create First_occurrence_of_string_in_a_String.cpp

---
+++ First_occurrence_of_string_in_a_String.cpp
@@ -0,0 +1,25 @@
+Time - O()
+Space - O()
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int m = haystack.length();
+        int n = needle.length();
+        int i=0;
+        while(i<=m-n){
+            if(haystack[i]==needle[0]){
+                int k = i;
+                int j = 0;
+                while(haystack[k]==needle[j]){
+                    k++;
+                    j++;
+                    if(j==n){
+                        return i;
+                    }
+                }
+            }
+            i++;
+        }
+        return -1;
+    }
+};
