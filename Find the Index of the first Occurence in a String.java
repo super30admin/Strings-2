776dfc264b7a501ed7db6c37e8b7c2009f00fe0c
Dimpleben Kanjibhai Patel <patel.dim@northeastern.edu>
2023-12-28 21:11:18 -0500

    Strings-2

--- Find the Index of the first Occurence in a String.java
+++ Find the Index of the first Occurence in a String.java
@@ -28,3 +28,31 @@ class Solution {
         return -1;
     }
 }
+
+// Bruteforce approach
+// Time Complexity: O(m * n)
+// Space Complexity: O(1)
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int m = haystack.length();
+        int n = needle.length();
+
+        if(m < n) return -1;
+
+        int i=0;
+        int j=0;
+
+        while( i<= m-n){
+            int k = i;
+            while(haystack.charAt(k) == needle.charAt(j)){
+                k++;
+                j++;
+                if(j==n) return i;
+            }
+            i++;
+            j=0;
+
+        }
+        return -1;
+    }
+}
