be381bb011f65530dde64d0decaa525fb0c5d6cb
pnayani75 <35510785+pnayani75@users.noreply.github.com>
2024-02-07 17:44:42 -0800

    Create FindAllAnagrams.java

---
+++ FindAllAnagrams.java
@@ -0,0 +1,41 @@
+// TC: o(m) SC: o(1)
+class FindAllAnagrams {
+    public List<Integer> findAnagrams(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        List<Integer> result = new ArrayList<>();
+        if(n>m) return result;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0 ; i<n;i++){
+            char c= p.charAt(i);
+            map.put(c, map.getOrDefault(c,0)+1);
+        }
+        int match = 0 ;
+        for(int i=0; i<m;i++){
+            char in  = s.charAt(i);
+            if(map.containsKey(in)){
+                int cnt = map.get(in);
+                cnt--;
+                if(cnt == 0){
+                    match++;
+                }
+                map.put(in, cnt);
+            }
+            if(i>=n){
+                char out = s.charAt(i-n);
+                if(map.containsKey(out)){
+                    int cnt = map.get(out);
+                    cnt++;
+                    if(cnt==1){
+                        match--;
+                    }
+                    map.put(out,cnt);
+                }
+            }
+            if(match == map.size()){
+                result.add(i-n+1);
+            }
+        }
+        return result;
+    }
+}
